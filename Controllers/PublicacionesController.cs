d161e91ddfd0ac32f90c227d3a24e8c1900a682e
Daniel Di Lorenzo <ddilorenzo@gmail.com>
2020-06-27 15:36:34 -0300

    Arreglo PATCH

--- Controllers/PublicacionesController.cs
+++ Controllers/PublicacionesController.cs
@@ -24,6 +24,7 @@ namespace IkePublicacionesMarketingService.Controllers
 
         [HttpPut("{id}")]
         [Authorize(Roles = "Admin")]
+
         public async Task<IActionResult> PutPublicacion(long id, Publicacion publicacion)
         {
             if (id != publicacion.Id)
@@ -52,11 +53,51 @@ namespace IkePublicacionesMarketingService.Controllers
             return NoContent();
         }
 
+        // PUT: api/Usuario/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPatch("{id}")]
+        [Authorize(Roles = "Admin")]
+
+        public async Task<IActionResult> PatchPublicacion(int id, [FromBody] JsonPatchDocument<Publicacion> PublicacionPatch)
+        {
+            Publicacion publicacion = await _context.Publicaciones.FirstOrDefaultAsync(u => u.Id == id);
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                PublicacionPatch.ApplyTo(publicacion);
+
+                _context.Entry(publicacion).State = EntityState.Modified;
+
+                await _context.SaveChangesAsync();
+
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublicacionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
         // POST: api/Publicaciones
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         [Authorize(Roles = "Admin")]
+
         public async Task<ActionResult<Publicacion>> PostPublicacion(Publicacion publicacion)
         {
             _context.Publicaciones.Add(publicacion);
